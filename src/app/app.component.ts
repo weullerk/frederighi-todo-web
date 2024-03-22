d56e59f83f2c8ef10b8c845f8ab30e2c97eb6dfd
Weuller Krysthian R Costa <weuller.krysthian@gmail.com>
2024-03-22 07:53:22 -0300

    Iniciando repositório com as telas de login, cadastro e lista de tarefas e o template default

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {LoginComponent} from "./components/login/login.component";
+import {CadastroComponent} from "./components/cadastro/cadastro.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginComponent, CadastroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/tarefas/tarefas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tarefas',
+  standalone: true,
+  imports: [],
+  templateUrl: './tarefas.component.html',
+  styleUrl: './tarefas.component.scss'
+})
+export class TarefasComponent {
+
+}
