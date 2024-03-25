e4500a8230aaf82317d1e28bac1e674a87ebcdf1
Weuller Krysthian R Costa <weuller.krysthian@gmail.com>
2024-03-25 07:38:40 -0300

    Adicionando rota dos componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,15 @@
 import { Routes } from '@angular/router';
 import {CadastroComponent} from "./components/cadastro/cadastro.component";
 import {HomeComponent} from "./components/home/home.component";
+import {TarefasComponent} from "./components/tarefas/tarefas.component";
+import {CadastrarTarefaComponent} from "./components/cadastrar-tarefa/cadastrar-tarefa.component";
+import {EditarTarefaComponent} from "./components/editar-tarefa/editar-tarefa.component";
+import {ExcluirTarefaComponent} from "./components/excluir-tarefa/excluir-tarefa.component";
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'cadastrar', component: CadastroComponent },
+  { path: 'cadastrar-tarefa', component: CadastrarTarefaComponent },
+  { path: 'editar-tarefa', component: EditarTarefaComponent },
+  { path: 'excluir-tarefa', component: ExcluirTarefaComponent },
 ];
